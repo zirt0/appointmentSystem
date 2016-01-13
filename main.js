3e1900344e5916851e929c99b3eadc6a021e8f1d
zirt0 <ak_adem@live.nl>
2016-01-13 23:19:28 +0100

    Naam toegevoegd

--- main.js
+++ main.js
@@ -3,16 +3,45 @@
 	app.config(function($routeProvider){
 
 		$routeProvider
-			.when('/',{
-				templateUrl:'partials/index.html',
-				controller:'customers'
-			})
+		.when('/',{
+			templateUrl:'partials/index.html',
+			controller:'dashboard'
+		})
 
-			.otherwise({
-				redirectTo:'/dashboard'
-			});
+		.otherwise({
+			redirectTo:'/dashboard'
+		});
 	});
 
-	app.controller('loginCtrl', function($scope){
+	app.controller('dashboard', function($scope){
+		function getNearestHalfHourTimeString() {
+			var now = new Date();
+			var hour = now.getHours();
+			var minutes = now.getMinutes();
+			var ampm = "AM";
+			if (minutes < 15) {
+				minutes = "00";
+			} else if (minutes < 45){
+				minutes = "30";
+			} else {
+				minutes = "00";
+				++hour;
+			}
+			if (hour > 23) {
+				hour = 12;
+			} else if (hour > 12) {
+				hour = hour - 12;
+				ampm = "PM";
+			} else if (hour == 12) {
+				ampm = "PM";
+			} else if (hour == 0) {
+				hour = 12;
+			}
+
+			return(hour + ":" + minutes + " " + ampm);
+		}
+
+		console.log(getNearestHalfHourTimeString());
+
 
 	});
